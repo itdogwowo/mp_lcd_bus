aac03df043b205e0c33851b0529719e301531b84
TVB-OA\bl91920 <itdogwowo@gmail.com>
2026-05-28 15:06:56 +0800

    no message

--- esp32_src/spi_bus.c
+++ esp32_src/spi_bus.c
@@ -182,6 +182,8 @@ static mp_obj_t spi_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_
         .spics_io_num = -1,
         .queue_size = SPI_DMA_QUEUE_DEPTH,
         .flags = dflags,
+        .command_bits = (self->lane_count >= 2) ? 8 : 0,
+        .address_bits = (self->lane_count >= 2) ? 24 : 0,
     };
 
     esp_err_t ret = spi_bus_initialize(self->host, &bcfg, SPI_DMA_CH_AUTO);
@@ -228,24 +230,20 @@ static mp_obj_t spi_write(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_
     if (args[ARG_cmd].u_int >= 0) {
         spi_drain_pending(self);
 
-        spi_transaction_ext_t t;
+        spi_transaction_t t;
         memset(&t, 0, sizeof(t));
-        uint32_t flags = lane_flag(self->lane_count);
         if (args[ARG_multiline].u_bool) {
-            flags |= SPI_TRANS_MULTILINE_CMD | SPI_TRANS_MULTILINE_ADDR;
+            t.flags |= SPI_TRANS_MULTILINE_CMD | SPI_TRANS_MULTILINE_ADDR;
         }
-        t.base.flags = flags;
-        t.command_bits = 8;
-        t.address_bits = 24;
-        t.base.cmd  = (uint16_t)args[ARG_cmd].u_int;
-        t.base.addr = (uint32_t)args[ARG_addr].u_int;
+        t.cmd  = (uint16_t)args[ARG_cmd].u_int;
+        t.addr = (uint32_t)args[ARG_addr].u_int;
 
         mp_buffer_info_t bufinfo;
         mp_get_buffer_raise(args[ARG_buf].u_obj, &bufinfo, MP_BUFFER_READ);
-        t.base.tx_buffer = bufinfo.buf;
-        t.base.length    = bufinfo.len * 8;
+        t.tx_buffer = bufinfo.buf;
+        t.length    = bufinfo.len * 8;
 
-        spi_device_polling_transmit(self->handle, (spi_transaction_t *)&t);
+        spi_device_polling_transmit(self->handle, &t);
         return mp_const_none;
     }
 
