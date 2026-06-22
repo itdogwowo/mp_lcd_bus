efb46eba01bf1bbb81f17aac83c4c98c0a8039b9
TVB-OA\bl91920 <itdogwowo@gmail.com>
2026-06-22 16:05:16 +0800

    no message

--- micropython.mk
+++ micropython.mk
@@ -13,3 +13,6 @@ SRC_USERMOD_C += $(MOD_DIR)/common_src/i2c_bus.c
 SRC_USERMOD_C += $(MOD_DIR)/common_src/i80_bus.c
 SRC_USERMOD_C += $(MOD_DIR)/common_src/spi_bus.c
 SRC_USERMOD_C += $(MOD_DIR)/common_src/rgb_bus.c
+
+# 強制設 max_transfer_sz 上限
+ESP_IDF_COMPONENT_CONFIG += CONFIG_SPI_MASTER_MAX_TRANSFER_LENGTH_BYTES=131072
